90c4bf43ed40fd562ec106480d45be81a3697856
Duy <nova411992@gmail.com>
2021-09-02 08:35:34 +0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,16 @@
+add new data 
+
+
+add_filter('woocommerce_shipstation_export_order_xml', 'd_add_new_data');
+function d_add_new_data($order_xml){
+
+  $doc = $order_xml->ownerDocument;
+
+  // create element
+  $data = $order_xml->appendChild( $doc->createElement( 'test' ) );
+
+  // add value
+  $data->appendChild($doc->createCDATASection('new') );
+
+  return $order_xml;
+}
